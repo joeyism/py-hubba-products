222a06b775ef3dcde0b8f6371fc70e8985db08f5
joeyism <sham.joey@gmail.com>
2018-01-17 16:37:06 -0500

    Updated to get recommendation

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from collections import defaultdict
+
 
 prods = pd.read_csv("prods.csv")
 actions = pd.read_csv("actions.csv")
@@ -26,17 +28,6 @@ for unused_col in ["deleteddate", "surl"]:
 
 
 prods = prods.drop_duplicates()
-prods2 = prods.groupby(["name"]).agg({
-    "category": {
-        "joined_category": lambda x: " ".join([y if isinstance(y, str) else str(y) for y in x ]),
-        "count": lambda x: len(x)
-    },
-    "id1": {"no_of_rows": "count"}
-}).reset_index()
-prods2.columns = ["name"] + list(prods2.columns.droplevel(0)[1:])
-
-del prods["category"]
-
 ### Actions
 for col in actions.columns:
     if actions[col].dtype == np.object:
@@ -48,9 +39,42 @@ for col in actions.columns:
 actions["context_product"] = actions["context_page_path"].str.split("/").str[-1]
 actions.rename(columns={"id": "actions_id"}, inplace = True)
 
-actions2 = actions[["user_id", "context_product", "action"]].groupby(["user_id", "context_product"]).count().reset_index()
+### Modify
+products_missing_from_prods = list(set(actions["context_product"].values) - set(prods["name"].values))
 
-### Buyers
+# for missing in prods2, scrape site
+#for name in products_missing_from_prods:
+#    actions_row = actions.loc[actions["context_product"] == name].iloc[0]
+#    url = actions_row["context_page_url"]
+#    #TODO: scrape and add to prods
+
+products_missing_df = pd.DataFrame(products_missing_from_prods, columns=["name"])
+prods = prods.append(products_missing_df).reset_index(drop=True)
+prods = prods.drop_duplicates()
+
+
+prods2 = prods.groupby(["name"]).agg({
+    "category": {
+        "joined_category": lambda x: " ".join([y if isinstance(y, str) else str(y) for y in x ]),
+        "count": lambda x: len(x)
+    },
+    "id1": {"no_of_rows": "count"}
+}).reset_index()
+prods2.columns = ["name"] + list(prods2.columns.droplevel(0)[1:])
+
+del prods["category"]
+
+positive_actions = ["Liked Product", "Added Product To List", "Followed User", "Followed Brand", "Followed List", "saved product"]
+
+actions2 = actions[["user_id", "context_product", "action"]].groupby(["user_id", "context_product"]).agg({
+    "action": {
+        "positive_action_count": lambda x: len([y for y in x if y in positive_actions])
+        }
+    }).reset_index()
+actions2.columns = ["user_id", "context_product"] + list(actions2.columns.droplevel(0)[2:])
+
+
+
 buyers2 = buyers.groupby(["owner"]).nunique()
 del buyers2["owner"]
 buyers2 = buyers2.reset_index()
@@ -62,6 +86,9 @@ del buyers3
 buyers2.columns = ["owner", "no_of_rows", "no_of_products", "no_of_categories"]
 
 
+
+
+
 #actions["user_id"] == buyers["owner"]
 # 5a4978af403d1a5afc74484a
 
@@ -70,16 +97,51 @@ buyers2.columns = ["owner", "no_of_rows", "no_of_products", "no_of_categories"]
 
 df = actions2.merge(buyers2, left_on="user_id", right_on="owner", how="left").merge(prods2, left_on="context_product", right_on="name", how="left")
 
-# for missing in prods2, scrape site
-#for name in df.loc[df["name"].isnull()]["context_product"].unique():
-#    actions_row = actions.loc[actions["context_product"] == name].iloc[0]
-#    url = actions_row["context_page_url"]
-#    #TODO: scrape and add to prods
 
 
 
 def action_timeline(user_id, product):
     return actions.loc[(actions["user_id"] == user_id) & (actions["context_product"] == product), ["user_id", "context_product", "action"]]
 
-actions_per_user = actions2[["user_id", "action"]].groupby("user_id").sum().reset_index() #55.335 actions
-actions_per_product = actions2[["context_product", "action"]].groupby("context_product").sum().reset_index()
+actions_per_user = actions2[["user_id", "positive_action_count"]].groupby("user_id").sum().reset_index() #55.335 actions
+actions_per_product = actions2[["context_product", "positive_action_count"]].groupby("context_product").sum().reset_index()
+
+
+from surprise import Reader, Dataset, SVD, evaluate, KNNBasic
+
+def get_top_n(predictions, n=10):
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
+
+reader = Reader(rating_scale = (0, 5))
+dataset = Dataset.load_from_df(actions2, reader)
+
+algo = SVD()
+
+
+evaluate(algo, dataset, measures=["RMSE", "MAE"])
+trainset = dataset.build_full_trainset()
+
+algo.fit(trainset)
+algo.predict("53ff5739aebb450829000074","affect-health-drinking-chocolate", 15)
+algo.predict("53ff5739aebb450829000074","affect-health-drinking-chocolate", 0)
+
+testset = trainset.build_anti_testset()
+predictions = algo.test(testset)
+
+top_n = get_top_n(predictions, n=10)
+
+recommendations = {}
+# Print the recommended items for each user
+for uid, user_ratings in top_n.items():
+    recommendations[uid]= [iid for (iid, _) in user_ratings]
---
+++ ec2.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y update
+sudo apt-get install -y python3-dev htop
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
