8a23c1d3605c4d21a0cedc6a4450889cdbe9fa76
joey sham <jsham@interac.ca>
2018-01-18 14:35:06 -0500

    Updated to include data that is to be stored

--- analysis.py
+++ analysis.py
@@ -1,113 +1,9 @@
-import pandas as pd
-import numpy as np
-from collections import defaultdict
-
-
-prods = pd.read_csv("prods.csv")
-actions = pd.read_csv("actions.csv")
-buyers = pd.read_csv("buyers.csv")
-
-
-def nan_1_and_0(df, only_value):
-    df.loc[df == only_value] = 1
-    df.fillna(0)
-
-
-### Prods
-
-# ['_id', 'b', 'c', 'createdat', 'd', 'deleted', 'deleteddate', 'n', 'o', 'p', 'slug', 'sp', 'surl', 'updatedat', 'deletedat', 'value']
-prods.columns = ["id1", "id2", "id3", "create_date", "description", "deleted", "deleteddate", "product_name", "id4", "id5", "name", "picture", "surl", "updated_date", "deleted_date", "category"]
-
-# remove columns with all NaNs
-for unused_col in ["deleteddate", "surl"]:
-    del prods[unused_col]
-
-# Split categories by __, and store
-#for i in range(3):
-#    prods["category"+str(i)] = prods["category"].str.split("__").str[i]
-
-
-prods = prods.drop_duplicates()
-### Actions
-for col in actions.columns:
-    if actions[col].dtype == np.object:
-        unique = actions[col].describe()["unique"]
-        if unique == 1:
-            top = actions[col].describe()["top"]
-            nan_1_and_0(actions[col], top)
-
-actions["context_product"] = actions["context_page_path"].str.split("/").str[-1]
-actions.rename(columns={"id": "actions_id"}, inplace = True)
-
-### Modify
-products_missing_from_prods = list(set(actions["context_product"].values) - set(prods["name"].values))
-
-# for missing in prods2, scrape site
-#for name in products_missing_from_prods:
-#    actions_row = actions.loc[actions["context_product"] == name].iloc[0]
-#    url = actions_row["context_page_url"]
-#    #TODO: scrape and add to prods
-
-products_missing_df = pd.DataFrame(products_missing_from_prods, columns=["name"])
-prods = prods.append(products_missing_df).reset_index(drop=True)
-prods = prods.drop_duplicates()
-
-
-prods2 = prods.groupby(["name"]).agg({
-    "category": {
-        "joined_category": lambda x: " ".join([y if isinstance(y, str) else str(y) for y in x ]),
-        "count": lambda x: len(x)
-    },
-    "id1": {"no_of_rows": "count"}
-}).reset_index()
-prods2.columns = ["name"] + list(prods2.columns.droplevel(0)[1:])
-
-del prods["category"]
-
-positive_actions = ["Liked Product", "Added Product To List", "Followed User", "Followed Brand", "Followed List", "saved product"]
-
-actions2 = actions[["user_id", "context_product", "action"]].groupby(["user_id", "context_product"]).agg({
-    "action": {
-        "positive_action_count": lambda x: len([y for y in x if y in positive_actions])
-        }
-    }).reset_index()
-actions2.columns = ["user_id", "context_product"] + list(actions2.columns.droplevel(0)[2:])
-
-
-
-buyers2 = buyers.groupby(["owner"]).nunique()
-del buyers2["owner"]
-buyers2 = buyers2.reset_index()
-
-buyers3 = buyers.groupby(["owner"]).count().reset_index()
-
-buyers2 = buyers3[["owner", "_id"]].merge(buyers2[["owner", "description", "value"]], on="owner")
-del buyers3
-buyers2.columns = ["owner", "no_of_rows", "no_of_products", "no_of_categories"]
-
-
-
-
-
-#actions["user_id"] == buyers["owner"]
-# 5a4978af403d1a5afc74484a
-
-#prods["name"] == actions["context_product"]
-# mastrad-paris-pro-gourmet-hotcold-whipper-sst-0.5l
-
-df = actions2.merge(buyers2, left_on="user_id", right_on="owner", how="left").merge(prods2, left_on="context_product", right_on="name", how="left")
-
-
-
-
-def action_timeline(user_id, product):
-    return actions.loc[(actions["user_id"] == user_id) & (actions["context_product"] == product), ["user_id", "context_product", "action"]]
-
-actions_per_user = actions2[["user_id", "positive_action_count"]].groupby("user_id").sum().reset_index() #55.335 actions
-actions_per_product = actions2[["context_product", "positive_action_count"]].groupby("context_product").sum().reset_index()
-
-
+import data
 from surprise import Reader, Dataset, SVD, evaluate, KNNBasic
+
+prods = data.get_prods()
+actions = data.get_actions()
+buyers = data.get_buyers()
 
 def get_top_n(predictions, n=10):
 
@@ -124,7 +20,7 @@ def get_top_n(predictions, n=10):
     return top_n
 
 reader = Reader(rating_scale = (0, 5))
-dataset = Dataset.load_from_df(actions2, reader)
+dataset = Dataset.load_from_df(actions[["user_id", "context_product"]], reader)
 
 algo = SVD()
 
--- data.py
+++ data.py
@@ -47,7 +47,10 @@ prods2 = prods.groupby(["name"]).agg({
         "joined_category": lambda x: " ".join([y if isinstance(y, str) else str(y) for y in x ]),
         "count": lambda x: len(x)
     },
-    "id1": {"no_of_rows": "count"}
+    "id1": {"no_of_rows": "count"},
+    "picture": {"picture": "first"},
+    "description": {"description": "first"},
+    "product_name": {"product_name": "first"},
 }).reset_index()
 prods2.columns = ["name"] + list(prods2.columns.droplevel(0)[1:])
 
@@ -55,7 +58,7 @@ del prods["category"]
 
 positive_actions = ["Liked Product", "Added Product To List", "Followed User", "Followed Brand", "Followed List", "saved product"]
 
-actions2 = actions[["user_id", "context_product", "action"]].groupby(["user_id", "context_product"]).agg({
+actions2 = actions.groupby(["user_id", "context_product"]).agg({
     "action": {
         "positive_action_count": lambda x: len([y for y in x if y in positive_actions])
         }
@@ -68,11 +71,16 @@ buyers2 = buyers.groupby(["owner"]).nunique()
 del buyers2["owner"]
 buyers2 = buyers2.reset_index()
 
-buyers3 = buyers.groupby(["owner"]).count().reset_index()
+buyers3 = buyers.groupby(["owner"]).agg({
+    "_id": {"_id": "count"},
+    "description": {"description": "first"},
+    "name": {"name": "first"}
+}).reset_index()
+buyers3.columns = ["owner"] + list(buyers3.columns.droplevel(0)[1:])
 
-buyers2 = buyers3[["owner", "_id"]].merge(buyers2[["owner", "description", "value"]], on="owner")
+buyers2 = buyers3[["owner", "_id", "description"]].merge(buyers2[["owner", "description", "value", "name"]], on="owner")
 del buyers3
-buyers2.columns = ["owner", "no_of_rows", "no_of_products", "no_of_categories"]
+buyers2.columns = ["owner", "no_of_rows", "description", "no_of_products", "no_of_categories", "name"]
 
 
 
@@ -92,13 +100,13 @@ actions_per_product = actions2[["context_product", "positive_action_count"]].gro
 
 
 def get_prods():
-    return prods
+    return prods2
 
 def get_actions():
     return actions2
 
 def get_buyers():
-    return buyers
+    return buyers2
 
 
 
